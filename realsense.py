a4b52f08f4d54f3b204e12404799a43a6ceeff71
Erik Bauer <erik.hamburg@gmx.de>
2022-03-23 22:48:29 +0100

    changed ret of get_frames() to np

--- realsense.py
+++ realsense.py
@@ -1,4 +1,6 @@
 import pyrealsense2 as rs
+import numpy as np
+
 
 class RSCamera:
     def __init__(self):
@@ -6,18 +8,16 @@ class RSCamera:
         config = rs.config()
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        
+
         self.pipeline.start(config)
-        sensor = self.pipeline.get_active_profile().get_device().query_sensors()[1]
+        sensor = self.pipeline.get_active_profile(
+        ).get_device().query_sensors()[1]
         sensor.set_option(rs.option.enable_auto_exposure, True)
-
-
 
     def get_frames(self):
         frames = self.pipeline.wait_for_frames()
-        depth = frames.get_depth_frame()
-        color = frames.get_color_frame()
+
+        depth = np.asanyarray(frames.get_depth_frame().get_data())
+        color = np.asanyarray(frames.get_color_frame().get_data())
 
         return (color, depth)
-
-
