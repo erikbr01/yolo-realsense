786da6246119336572a852ae50989e904fbd2cef
Erik Bauer <erik.hamburg@gmx.de>
2022-04-10 08:31:05 -0400

    added more comments and changed log file names

--- realsense.py
+++ realsense.py
@@ -5,6 +5,7 @@ import numpy as np
 class RSCamera:
     def __init__(self):
         self.pipeline = rs.pipeline()
+        # Dimensions for any camera of the Realsense series
         self.width = 640
         self.height = 480
         config = rs.config()
@@ -18,9 +19,13 @@ class RSCamera:
         depth_sensor = profile.get_device().first_depth_sensor()
         image_sensor = profile.get_device().query_sensors()[1]
 
+        # Need this to compute depth
         self.depth_scale = depth_sensor.get_depth_scale()
+
+        # Enable automatic change of expose for high contrast scenes
         image_sensor.set_option(rs.option.enable_auto_exposure, True)
 
+    # Get the raw frames as tensors
     def get_raw_frames(self):
         frames = self.pipeline.wait_for_frames()
 
@@ -29,12 +34,14 @@ class RSCamera:
 
         return (color, depth)
 
+    # Get the frames as rs2.Frame objects
     def get_rs_frames(self):
         frames = self.pipeline.wait_for_frames()
 
         depth = frames.get_depth_frame()
         color = frames.get_color_frame()
 
+    # Get frames aligned to color as tensors
     def get_raw_color_aligned_frames(self):
         frames = self.pipeline.wait_for_frames()
 
@@ -47,6 +54,7 @@ class RSCamera:
 
         return (color, depth)
 
+    # Get frames aligned to color as rs2 frame objects
     def get_rs_color_aligned_frames(self):
         frames = self.pipeline.wait_for_frames()
 
